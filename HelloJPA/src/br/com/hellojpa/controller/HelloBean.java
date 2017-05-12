d39b8497b438ab660886da8d18f55f85ec21b40e
Daniel Alencar <danieldgt@gmail.com>
2017-05-12 11:05:36 -0300

    .

--- HelloJPA/src/br/com/hellojpa/controller/HelloBean.java
+++ HelloJPA/src/br/com/hellojpa/controller/HelloBean.java
@@ -1,22 +1,51 @@
 package br.com.hellojpa.controller;
 
-
-import java.io.Serializable;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.hellojpa.dao.UsuarioDAO;
+import br.com.hellojpa.model.Usuario;
+
 @ManagedBean
 @SessionScoped
-public class HelloBean implements Serializable {
+public class HelloBean {
 
-	private static final long serialVersionUID = 1L;
+
+	public HelloBean() {
+//		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuario = new Usuario();
+//		usuario = usuarioDAO.encontrar(14);
+		usuario.setNome("joao");
+	}
+
+	
+	private Usuario usuario;
 
 	private String name;
+
+	private String nomeUsuario;
+
+	public String getNomeUsuario() {
+		nomeUsuario = usuario.getNome();
+		return nomeUsuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
 
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
